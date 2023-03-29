72b93b3300189281e4a037351a4135d5171157e6
Claudionor <claudionorojr@hotmail.com>
2023-03-29 12:47:50 -0300

    Initialized repository

---
+++ README.md
@@ -0,0 +1,39 @@
+# Ignite Timer
+
+> Projeto 02: um timer baseado na técnica de pomodoro.
+
+## Tecnologias
+
+- Date-fns
+
+- Eslint
+
+- Immer
+
+- Phosphor-react
+
+- React
+
+- React-hook-form
+
+```bash
+# Integração com bibliotecas de validação
+npm i @hookform/resolvers
+```
+
+- React-router-dom
+
+- Styled-components
+
+- TypeScript
+
+- Vite
+
+- Zod
+
+### Comandos
+
+```bash
+# Identificar arquivos com as extensões '.ts' e '.tsx' na pasta 'src' que estão fora do padrão de formatação do eslint.
+npx eslint src --ext .ts,.tsx
+```
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,72 @@
+import { HandPalm, Play } from 'phosphor-react'
+import { useContext } from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { FormProvider, useForm } from 'react-hook-form'
+import * as zod from 'zod'
+import { NewCycleForm } from './NewCycleForm'
+import { Countdown } from './Countdown'
+
+import {
+  HomeContainer,
+  StartCountdownButton,
+  StopCountdownButton,
+} from './styles'
+import { CyclesContext } from '../../contexts/CyclesContext'
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo deve ser de no mínimo 5 minutos.')
+    .max(60, 'O ciclo deve ser de no máximo 60 minutos.'),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+export function Home() {
+  const { createNewCycle, activeCycle, interruptCurrentCycle } =
+    useContext(CyclesContext)
+
+  const newCycleForm = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  const { handleSubmit, watch, reset } = newCycleForm
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data)
+
+    // ? O reset funciona voltando os campos para o que foi definido no 'defaultValues'
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
+  return (
+    <HomeContainer>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
+        <FormProvider {...newCycleForm}>
+          <NewCycleForm />
+        </FormProvider>
+        <Countdown />
+
+        {activeCycle ? (
+          <StopCountdownButton onClick={interruptCurrentCycle} type="button">
+            <HandPalm size={24} />
+            Interromper
+          </StopCountdownButton>
+        ) : (
+          <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+            <Play size={24} />
+            Começar
+          </StartCountdownButton>
+        )}
+      </form>
+    </HomeContainer>
+  )
+}
